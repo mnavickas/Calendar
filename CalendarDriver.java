d5ac1e36d4c1f4bfce6bf2853cbeeff4ed59b6ec
James Muoghalu <jmuoghalu00@outlook.com>
2016-09-15 00:00:53 -0500

    Updated CalendarDriver
    Updates:
    1.) There is now a for loop that organizes all of the days into their respective weeks.
         (This should be moved over to the Week class at some point and used in that class' functionality.)
    
    2.) Each date has a string title, which should be useful in knowing which database entries to select, and knowing which days to display for a given display mode.

--- CalendarDriver.java
+++ CalendarDriver.java
@@ -14,13 +14,25 @@ public class CalendarDriver
                 JFrame calendarMenu = createMenu();
 
 
-                // All calendar months, weeks, and days from August to May
-                // all twelve months are listed for ease in remembering indices
-                String[] allMonths = new String[] {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"};
-
+                // All calendar months, weeks, and days from August to May --> (length of 10)
+                String[] allAcademicMonths = new String[] {"Aug", "Sept", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May"};
 
                 // first week is the week that contains Monday August 1st, and the last week is the week that contains Friday May 12th
-                String[][] allAcademicWeeks = new String[41][7];
+                String[][] allAcademicWeeks = new String[44][7];
+
+                // initializes weeks array
+                for(int A = 0 ; A < allAcademicWeeks.length ; A++)
+                {
+                        for(int B = 0 ; B < allAcademicWeeks[A].length ; B++)
+                        {
+                                allAcademicWeeks[A][B] = null;
+                        }
+                }
+
+                // will contain all dates between August 1st and May 31st (inclusive) in string form
+                String[] allAcademicDays = new String[306];
+
+
 
 
                 /*
@@ -35,14 +47,15 @@ public class CalendarDriver
                                 academicYear[A][B] = null;
                         }
                 }
-*/
+                */
+
+
+
 
 
                 /*****
                         Days Array
                 *****/
-                // will contain all dates between August 1st and May 31st (inclusive) in string form
-                String[] allAcademicDays = new String[306];
 
                 // for filling the allAcademicDays array
                 int curDayIndex = 0;
@@ -51,42 +64,79 @@ public class CalendarDriver
                 // will turn all calendar dates (outside of June and July) into strings in the form DDMMMM for September and DDMMM for the other months
 
 
-                for(int A = 1 ; A <= 12 ; A++)
+                for(int A = 0 ; A < allAcademicMonths.length ; A++)
                 {
-                        if(A != 6 && A != 7)
+
+                        String curMonthName = allAcademicMonths[A];
+                        String curDate = "";
+
+                        // first for loop limited to 28 days for February 2017
+                        for(int B = 1 ; B <= 28 ; B++)
                         {
-                                String curMonthName = allMonths[A-1];
-                                String curDate = "";
 
-                                for(int B = 1 ; B <= 30 ; B++)
+                                // adds a zero to the beginning of the number string so that it has two digits representing the date
+                                if(B < 10)
                                 {
-
-                                        // adds a zero to the beginning of the number string so that it has two digits representing the date
-                                        if(B < 10)
-                                        {
-                                                curDate = "0" + Integer.toString(B) + curMonthName;
-                                        }
-                                        else
-                                        {
-                                                curDate = Integer.toString(B) + curMonthName;
-                                        }
-
-                                        // adds the date to an array of strings
-                                        allAcademicDays[curDayIndex] = curDate;
-                                        curDayIndex++;
-
+                                        curDate = "0" + Integer.toString(B) + curMonthName;
+                                }
+                                else
+                                {
+                                        curDate = Integer.toString(B) + curMonthName;
                                 }
 
-                                // for months with 31 days
-                                if( A == 1 || A == 3 || A == 5 || A == 8 || A == 10 || A == 12 )
-                                {
-                                        curDate = Integer.toString(31) + curMonthName;
-                                        allAcademicDays[curDayIndex] = curDate;
-                                        curDayIndex++;
-                                }
+                                // adds the date to an array of strings
+                                allAcademicDays[curDayIndex] = curDate;
+                                curDayIndex++;
 
                         }
+
+                        // to fill the months with at least 30 days
+                        if( curMonthName != "Feb" )
+                        {
+                                curDate = Integer.toString(29) + curMonthName;
+                                allAcademicDays[curDayIndex] = curDate;
+                                curDayIndex++;
+
+                                curDate = Integer.toString(30) + curMonthName;
+                                allAcademicDays[curDayIndex] = curDate;
+                                curDayIndex++;
+                        }
+
+                        // for months with 31 days
+                        if( curMonthName == "Aug" || curMonthName == "Oct" || curMonthName == "Dec" || curMonthName == "Jan" || curMonthName == "Mar" || curMonthName == "May" )
+                        {
+                                curDate = Integer.toString(31) + curMonthName;
+                                allAcademicDays[curDayIndex] = curDate;
+                                curDayIndex++;
+                        }
+
                 }
+
+
+
+
+                /*****
+                        Weeks Array
+                *****/
+
+                curDayIndex = 0;
+                for(int A = 0 ; A < allAcademicWeeks.length ; A++)
+                {
+                        for(int B = 0 ; B < allAcademicWeeks[A].length ; B++)
+                        {
+                                // skips over July 31st, 2016, which lies on a Sunday
+                                if(A == 0 && B == 0) {;}
+
+                                else if(curDayIndex < allAcademicDays.length)
+                                {
+                                        allAcademicWeeks[A][B] = allAcademicDays[curDayIndex];
+                                        curDayIndex++;
+                                }
+                        }
+                }
+
+
+
 
 
 
@@ -96,9 +146,13 @@ public class CalendarDriver
 
                 System.out.println("Printing Days Array:");
 
-                for(int A = 0 ; A < allAcademicDays.length ; A++)
+                for(int A = 0 ; A < allAcademicWeeks.length ; A++)
                 {
-                        System.out.println(allAcademicDays[A]);
+                        System.out.println("\n\nWeek: " + A);
+                        for(int B = 0 ; B < allAcademicWeeks[A].length ; B++)
+                        {
+                                System.out.print(allAcademicWeeks[A][B] + ", ");
+                        }
                 }
 
 
@@ -139,7 +193,9 @@ public class CalendarDriver
 
         Calendar Dates:
 
-        * 42 weeks from August 1st to May 12th
+        * 44 weeks from August 1st to May 31st
+        * Note: July 31st is set to null
+        * Calendar work should start with week three (allAcademicWeeks index two)
 
 
 
