635d62d070ae32605429a99d1b830b02139a7726
James Muoghalu <jmuoghalu00@outlook.com>
2016-09-18 16:19:38 -0500

    Added documentation to YearView

--- src/YearView.java
+++ src/YearView.java
@@ -16,15 +16,16 @@ import java.util.GregorianCalendar;
 
 
 
-
 /**
- * YearView shows all of the months in an academic year.
+ * YearView class
  *
- * MonthView is embedded within it.
- */
+ * The YearView will display all ten possible instances of MonthView in a single frame.
+ * The dates within each month can be clicked by the user, allowing the user to edit the day's details.
+*/
 public class YearView extends JPanel
 {
 
+        // member variables used by the class
         MonthView[] months;
         JPanel panelNorth;
         JPanel panelCenter;
@@ -35,42 +36,48 @@ public class YearView extends JPanel
         String[] days;
 
 
-
         /**
-         * Create the year view window.
-         * @param db The database to use.
-         */  
-        public static void initialize(DBManager db)
+         * Creates an instance of the YearView within a JFrame
+         * @return none
+        */
+        public static void initialize()
         {
                 JFrame frame = new JFrame("Year View");
 
                 frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-                YearView view = new YearView(db);
+
+                // returns to the main start menu
+                YearView view = new YearView();
                 JButton button = new JButton("BACK");
                 frame.add(button,BorderLayout.NORTH);
                 frame.add(view, BorderLayout.CENTER);
                 frame.pack();
                 frame.setVisible(true);
-              button.addActionListener(new ActionListener()
-              {
+
+                // action that occurs when the back button is selected
+                button.addActionListener(new ActionListener()
+                {
 
                       public void actionPerformed(ActionEvent arg0)
                       {
                         StartMenu.initialize();
                         frame.dispose();
                       }
-              });
+                });
+
         }
 
-
         /**
-         * Create the year view.
-         * @param db The database to use.
-         */  
-        public YearView(DBManager db)
+         * Yearview constructor
+         * @param none
+        */
+        public YearView()
         {
+
                 super( new BorderLayout() );
+
+		DBManager db = new DBManager("testuser");
 
                 this.panelNorth = new JPanel( new GridLayout(2,1) );
 
@@ -81,31 +88,34 @@ public class YearView extends JPanel
                 // will help with alternating the sizes of adjacent panels
                 int whichPanel = 1;
 
-
+                // creates the view's label and adds it to the frame
                 this.yearViewTitle = new JLabel("Academic School Year: 2016/2017");
                 panelNorth.add(yearViewTitle);
                 panelNorth.add( new JLabel("") );
                 add(panelNorth, BorderLayout.NORTH);
 
-
+                // useful member variables for labeling the month instances
                 this.academicMonths = new int[] { Calendar.AUGUST , Calendar.SEPTEMBER , Calendar.OCTOBER , Calendar.NOVEMBER , Calendar.DECEMBER , Calendar.JANUARY , Calendar.FEBRUARY , Calendar.MARCH , Calendar.APRIL , Calendar.MAY };
 
                 this.academicMonthsStrings = new String[] { "August" , "September" , "October" , "November" , "December" , "January" , "February" , "March" , "April" , "May" };
 
                 this.days = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
-
-
                 this.months = new MonthView[10];
                 int curYear = 2016;
 
+
+
+                // interates through array of MonthView instances that will be placed within the JFrame
                 for(int A = 0 ; A < 10 ; A++)
                 {
+                        // for titling the months correctly
                         if(A >= 5)
                         {
                                 curYear = 2017;
                         }
 
+                        // creates the month and adds it to the frame
                         this.months[A] = new MonthView( new Month(curYear, academicMonths[A]), false, db );
 
                         JPanel panel1 = new JPanel(new GridLayout(1,1));
@@ -115,7 +125,7 @@ public class YearView extends JPanel
 
 
 
-
+                // adds an empty gap between adjacent month panels in the frame
                 for(int A = 0 ; A < 10 ; A++)
                 {
                         this.panelCenter.add( months[A] );
@@ -123,7 +133,6 @@ public class YearView extends JPanel
                                 this.panelCenter.add( new JPanel( new GridLayout(1,1) ) );
                         }
                 }
-
                 add(this.panelCenter, BorderLayout.CENTER);
 
 
