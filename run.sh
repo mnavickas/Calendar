3b862680745005dc968408cf00eee00281a578ab
Matthew Bauer <mjbauer95@gmail.com>
2016-09-18 12:29:05 -0500

    Fix run script for linux.

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 cd src
 javac -d . *.java
-java -cp '.;../lib/mysql-connector-java-5.1.39-bin.jar' calendar.StartMenu
+java -cp .:../lib/mysql-connector-java-5.1.39-bin.jar calendar.StartMenu
 cd ..
