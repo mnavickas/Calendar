6093697890dc6e0bfa59bf81315e2723644bfef2
Matthew Bauer <mjbauer95@gmail.com>
2016-09-18 13:09:30 -0500

    Add back/next buttons for weeks.

--- src/WeekView.java
+++ src/WeekView.java
@@ -10,6 +10,8 @@ import javax.swing.JPanel;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -31,18 +33,22 @@ public class WeekView extends JPanel {
      * @parma day The day to use for the week.
      */
     public static void initialize(int year, int month, int day) {
-	JFrame frame = new JFrame("Day View");
-
-	DBManager db = new DBManager("testuser");
-
 	// initialize the week from params
 	Calendar firstDay = new GregorianCalendar(year, month, day);
+	DBManager db = new DBManager("testuser");
 	Week week = new Week(firstDay, db);
+
+	initialize(week);
+    }
+
+    public static void initialize(Week week) {
+	JFrame frame = new JFrame(week.toString());
+
 	WeekView view = new WeekView(week, true, false);
 	frame.add(view, BorderLayout.CENTER);
 
 	// back button
-	JButton button = new JButton("BACK");
+	JButton button = new JButton("Back to main menu");
 	frame.add(button,BorderLayout.NORTH);
 	button.addActionListener(new ActionListener()
 	    {
@@ -68,13 +74,48 @@ public class WeekView extends JPanel {
     public WeekView(Week week, boolean needsHeader, boolean needsText) {
 	super(new BorderLayout());
 
-	// setup a header with each month
-	JPanel panel1 = new JPanel(new GridLayout(1, 7));
-	for (String m : months) {
-	    JLabel label = new JLabel(m);
-	    panel1.add(label);
+	JPanel parent = this;
+
+	if (needsHeader) {
+	    JPanel panel4 = new JPanel(new BorderLayout());
+
+	    JButton prevButton = new JButton("<-");
+	    prevButton.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent arg0) {
+			// http://stackoverflow.com/questions/9650874/java-swing-obtain-window-jframe-from-inside-a-jpanel
+			JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(parent);
+			topFrame.dispose();
+
+			initialize(week.getPreviousWeek());
+		    }
+		});
+	    panel4.add(prevButton, BorderLayout.WEST);
+
+	    JButton nextButton = new JButton("->");
+	    nextButton.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent arg0) {
+			JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(parent);
+			topFrame.dispose();
+
+			initialize(week.getNextWeek());
+		    }
+		});
+
+	    panel4.add(nextButton, BorderLayout.EAST);
+
+	    JLabel weekLabel = new JLabel(week.toString(), SwingConstants.CENTER);
+	    panel4.add(weekLabel, BorderLayout.CENTER);
+
+	    // setup a header with each month
+	    JPanel panel1 = new JPanel(new GridLayout(1, 7));
+	    for (String m : months) {
+		JLabel label = new JLabel(m);
+		panel1.add(label);
+	    }
+	    panel4.add(panel1, BorderLayout.SOUTH);
+
+	    add(panel4, BorderLayout.NORTH);
 	}
-	add(panel1, BorderLayout.NORTH);
 
 	// setup a 7 grid pieces
 	JPanel panel2 = new JPanel(new GridLayout(1, 7));
